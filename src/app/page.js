576cf2eab903a4f488956bc7af85aab57832f5c5
T080 <T080@CIPL-ATC00263.colanonline.local>
2025-05-12 19:19:16 +0530

    Modified:UIi in the currency converter.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,24 @@
 
 import { useEffect, useState } from "react";
 import styles from "./page.module.scss";
+import Converter from "./Component/Converter/Converter";
+import { currencyService } from "./Service/currencyService";
+import { CurrencyArray } from "./Component/CurrencyName/CurrencyArray";
+
+
 
 export default function CurrencyConverter() {
+
   const [amount, setAmount] = useState(1);
   const [from, setFrom] = useState("USD");
   const [to, setTo] = useState("INR");
-  // const [result, setResult] = useState(null);
+  const [result, setResult] = useState(null);
 
   const convert = async () => {
-    const res = await fetch(
-      `https://v6.exchangerate-api.com/v6/5a454e3ce7ae5f37ad317ee0/latest/ALL`
-    );
+    const res = await currencyService();
     console.log(res.conversion_rates.AED);
+    console.log(CurrencyArray.code);
+    
     
     const data = await res.json();
     setResult(data.result.toFixed(2));
@@ -27,33 +33,12 @@ export default function CurrencyConverter() {
     <div >
       <h1 className={styles.heading}>Currency Converter</h1>
       <div className={styles.container}>
-        <label>Amount</label>
-        <input
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-
-        <label>From</label>
-        <select value={from} onChange={(e) => setFrom(e.target.value)}>
-          <option>USD</option>
-          <option>EUR</option>
-          <option>GBP</option>
-          <option>INR</option>
-        </select>
-
-        <label>To</label>
-        <select value={to} onChange={(e) => setTo(e.target.value)}>
-          <option>INR</option>
-          <option>USD</option>
-          <option>EUR</option>
-          <option>GBP</option>
-        </select>
+       <Converter/>
       </div>
 
-      {/* <div className={styles.result}>
+      <div className={styles.result}>
         {result && `${amount} ${from} = ${result} ${to}`}
-      </div> */}
+      </div>
     </div>
   );
 }
