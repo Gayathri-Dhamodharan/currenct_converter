4832f483f672e8e85fb732f13ad55addb680f7f1
T080 <T080@CIPL-ATC00263.colanonline.local>
2025-05-13 19:21:31 +0530

    Modified: Swap the currency and fetch the data from api

--- src/app/Component/Amount/Amount.jsx
+++ src/app/Component/Amount/Amount.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from "react";
+import { CurrencyArray } from "../CurrencyName/CurrencyArray";
 
-const Amount = () => {
+const Amount = ({ result, amount, from, to,styles }) => {
+  const fromCurrency = CurrencyArray.find((c) => c.code === from);
+  const toCurrency = CurrencyArray.find((c) => c.code === to);
+  console.log(styles,"styles");
+
+
   return (
     <div>
-        
-      
+      {result && (
+        <div className={styles.resultvalue}>
+          <p>
+            {amount}({fromCurrency?.code}){fromCurrency?.name || from}={result}(
+            {toCurrency?.code}){toCurrency?.name || to}
+          </p>
+          <p></p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Amount
+export default Amount;
