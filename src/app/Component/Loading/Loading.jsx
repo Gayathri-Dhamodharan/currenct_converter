bdea4e383505f9c6e0c6382f701362817f3a20ed
Gayathri-Dhamodharan <gayathridhamodharan7321@gmail.com>
2025-05-14 08:24:24 +0530

    Modified: Added currency country map and gif for loading

---
+++ src/app/Component/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import videoStyle from './'
+
+const Loading = () => {
+  return (
+     <div className="flex justify-center items-center h-screen">
+      <img src="/money.gif" alt="Loading animation" />
+    </div>
+  )
+}
+
+export default Loading
